bdaa68bb55ac41219b5dc8f47e105c8ca8f6ac07
pioneer-d <pioneer-d@naver.com>
2023-09-18 09:41:18 +0900

    [23.09.18] Feat : Lamda Expression
    기초적인 Lamda Expresison

--- app/src/main/java/com/junyoung/kotlinexercise/Lamda.kt
+++ app/src/main/java/com/junyoung/kotlinexercise/Lamda.kt
@@ -1,4 +1,11 @@
 
-fun main(){
 
+fun main(){
+    val lamda1 = { println("lamda Test") }
+    lamda1()
+    lamda1.invoke()
+
+    val lamda2: (Int, Int) -> Int = { a: Int, b: Int -> a+b }
+//    val lamda2: (Int, Int) -> Int = { a, b -> a+b }
+    // val lamda2 = {a: Int, b: Int -> a+b}
 }
