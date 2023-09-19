8188e484101ac819a2ce0c2ee2bd7e8688966f34
pioneer-d <pioneer-d@naver.com>
2023-09-20 08:46:55 +0900

    [23.09.18] Feat : Lamda Expression
    Lamda의 4가지 선언 방식

--- app/src/main/java/com/junyoung/kotlinexercise/Lamda.kt
+++ app/src/main/java/com/junyoung/kotlinexercise/Lamda.kt
@@ -1,11 +1,28 @@
 
 
 fun main(){
-    val lamda1 = { println("lamda Test") }
-    lamda1()
-    lamda1.invoke()
 
-    val lamda2: (Int, Int) -> Int = { a: Int, b: Int -> a+b }
+    // Lamda 선언 방식
+
+    // Parameter O, Return O
+    val poro: (Int, Int) -> Int = { a: Int, b: Int -> a+b }
 //    val lamda2: (Int, Int) -> Int = { a, b -> a+b }
-    // val lamda2 = {a: Int, b: Int -> a+b}
+//     val lamda2 = {a: Int, b: Int -> a+b}
+
+    // Parameter O, Return X
+    val porx: (Int, Int) -> Unit = { a,b -> println(a+b) }
+//    val porx = { a: Int, b: Int -> println(a+b) }
+
+    // Parameter X, Return O
+    val pxro: () -> String = { "No parameter but Return" }
+
+    // Parameter X, Return X
+    val pxrx: () -> Unit = { println("No parameter, No Return") }
+//     val pxrx = { println("") }
+
+
+    // Lamda 실행 방식
+    poro.invoke(1,2)
+    poro(1,2)
+    pxrx.invoke()
 }
